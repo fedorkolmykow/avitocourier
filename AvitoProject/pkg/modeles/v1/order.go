ecc70984f4a26cfc10f49160819f930337a5689f
fedorkolmykow <fedorkolmykow@gmail.ru>
2020-06-17 17:27:26 +0300

    Убрал пробелы перед omitempty

--- AvitoProject/pkg/modeles/v1/order.go
+++ AvitoProject/pkg/modeles/v1/order.go
@@ -3,21 +3,21 @@ package v1
 // Order ...
 type Order struct {
 	OrderID int           `json:"order_id"`
-	SellerPhone string    `json:"seller_phone, omitempty"`
+	SellerPhone string    `json:"seller_phone,omitempty"`
 	SellerName string     `json:"seller_name"`
-	BuyerPhone string     `json:"buyer_phone, omitempty"`
+	BuyerPhone string     `json:"buyer_phone,omitempty"`
 	BuyerName string      `json:"buyer_name"`
-	CourierPhone string   `json:"courier_phone, omitempty"`
-	CourierName string    `json:"courier_name, omitempty"`
-	City string           `json:"city, omitempty"`
-	StartAddr string      `json:"start_addr, omitempty"`
-	EndAddr string        `json:"end_addr, omitempty"`
+	CourierPhone string   `json:"courier_phone,omitempty"`
+	CourierName string    `json:"courier_name,omitempty"`
+	City string           `json:"city,omitempty"`
+	StartAddr string      `json:"start_addr,omitempty"`
+	EndAddr string        `json:"end_addr,omitempty"`
 	Title string          `json:"title"`
 	Price string          `json:"price"`
 	DeliveryPrice string  `json:"delivery_price"`
 }
 
-
+// OrderCreation ...
 type OrderCreation struct{
 	BuyerID int           `json:"buyer_id"`
 	EndAddrID int         `json:"end_addr_id"`
